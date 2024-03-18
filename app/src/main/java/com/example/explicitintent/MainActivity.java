3653c6a1dc403f77128558ef4c6eef4024a8a20e
keerthysesha <125575936+keerthysesha@users.noreply.github.com>
2024-03-18 14:07:01 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/explicitintent/MainActivity.java
+++ app/src/main/java/com/example/explicitintent/MainActivity.java
@@ -1,8 +1,11 @@
-package com.example.explicitintent;
+package com.example.explicitintend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void newScreen(View view){
+        Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+        startActivity(i);
     }
 }
